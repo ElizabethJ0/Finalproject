41970e3208e70baa8e2fe12a4f1086b439658849
ElizabethJ0 <116313967+ElizabethJ0@users.noreply.github.com>
2023-02-09 22:59:44 +0000

    2/9

--- 20's.py
+++ 20's.py
@@ -1,9 +1,11 @@
-user = input("Enter username: ").lower()
+user = input("Enter username: ")
 print(user +" you're in the 1920's")
 print("Do you want to go back to the present day?")
 choice = input("Choose Yes or No: ").lower()
 #user = input("Enter username: ").lower()
-#if user input("Yes").lower():
+
+if choice == "Yes":
+  
 
   #   print("You must complete the next 5 task correctly")
   # else
