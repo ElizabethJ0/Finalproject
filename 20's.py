1e04728327ec27c250732472480f1ae12657105c
ElizabethJ0 <116313967+ElizabethJ0@users.noreply.github.com>
2023-02-26 05:17:07 +0000

    2/25

--- 20's.py
+++ 20's.py
@@ -12,13 +12,13 @@ def train():
   if location == "boston":
     boston()
   else:
-      print("You feel asleep on the train. The next and last stop on this train is Boston.")
+      print("You feel asleep on the train. The next and last stop on this train is Boston. Have fun getting back to the present day in Boston. ")
       boston()
     
 def boston():    
     print("You just got off the train in Boston ")
-    activity = input("Do you want to go to a baseball game or jazz club?")
-    if activity == "baseball game" or activity == "baseball":
+    activity = input("Do you want to go to a baseball game or jazz club? ")
+    if activity == "baseball game" or activity == "baseball" or activity == "b":
       print("You are watching the baseball and need to find a device to take you back to the present day. You do not find the device but when you look up you are hit by a baseball, instantly taking you back to the present day. ")
     else:
       print("You're in the jazz club when you are pulled on stage, now forced to play one of four instruments.")
@@ -48,15 +48,28 @@ def boston():
 
     
 def horse():
-  print("Horse")
+    print(user + " you want to ride a horse? Well, you decide to ride a horse to get back to the present day. You go into the dark woods when you find a two-way road. Man, it's almost like you're playing a game with how many story-dependent choices you have to make.")
+
+    road = input("Looking both ways, do you go right or left?").lower()
+
+    if road == "left":
+
+      print(" I hope you know where you're going, because you chose to go left. Even if you did know where you were going, you apparently didn't read the sign that clearly said: 'Watch out for wolves.' Or maybe you did it on purpose, because what kind of person goes for a horse ride the second they unexpectedly go back in time? I think you know what happens next. You kind of deserved that.")
+
+    elif road == "right":
+      print(" Did you choose this at random, or was it because of the saying 'right is right'? Either way, you got lucky and traveled through the cartoonishly scary woods, emerging in the real world on the other side. How did this even happen? Well, you made it home.")
+
+    else:
+      print("That's not an option... can you read?")
+
+
 
 
   
 while loop:
-  print("Do you want to go back to the present day?")
-  choice = input("Choose Yes or No: ").lower()
-  if choice == "yes":
-    travel = input("Do you want to use a Train or Horse to get back to the present day? ").lower()
+  choice = input("Do you want to go back to the present day? ").lower()
+  if choice == "yes" or choice == "y":
+    travel = input("To get back to the present day you have two options. You can either ride a horse or train? Which one do you prefer? ").lower()
     if travel == "train":
       train()
       loop = False
@@ -66,16 +79,22 @@ while loop:
     else:
       print("Invalid")
       loop = False
-  elif choice == "no":
+  elif choice == "no" or choice == "nope":
     print("To stay you will have to answer all three questions correctly. If you don't answer correctly...")
     riddle = input("A sphere has three, a circle has two, a point has zero. What is it?\n A.Sides\n B.Line\n C.Dimensions\n D.All of the above\n").lower()
     if riddle == "dimensions" or riddle == "c":
       print("Lucky guess. You get to move onto the next question.")
       cereal = input("Is cereal a soup? ").lower()
       if cereal == "no":
-        print("Correct, you have the right opinion. Lets see if you can answer the next question correctly.")
-      else:
-        print("Really? It's not a soup. For that you are being sent to the dinosaur age, have fun with the meteor!")
+        print("Correct, you have the right opinion. Lets see if you ecan answer the next question correctly.")
+      # elif cereal == "yes" or cereal == "y":
+      #   print("Really? It's not a soup. For that you are being sent to the dinosaur age, have fun with the meteor!")
+        kevin = input("Who is the main character in Home Alone? ").lower()
+        if kevin == "kevin":
+            print("Good job " + user + " you get to stay in the 1920's. Until we see you again!")
+        else:
+          print("How did you mess that one up? With that you can't stay in the 1920's anymore and have to be sent back to the present day. Have a good life!")
+      
         
       
       
